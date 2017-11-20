9f466a315b2773e63268f0e7f88e458c8b31ae12
Technopriest90 <erokhin.ag@yandex.ru>
2017-11-20 15:51:48 +0300

    Добавил jsdoc

--- js/stat.js
+++ js/stat.js
@@ -22,10 +22,15 @@ window.renderStatistics = function (ctx, names, times) {
   drawRectangle(axisXTable, axisYTable, widthTable, heightTable, stepImage, colorTable1, colorTable2);
   drawText(text);
   drawHistogram(histogramHeight, barWidth, margin);
-
-  function drawHistogram(height, width, marginLR) {
-    var indent = (((widthTable + 20) - times.length * width - marginLR * 2) / (times.length - 1)) + width;
-    var initialX = axisXTable + marginLR; // px;
+  /**
+   * Draws a histogram.
+   * @param {string} height - Height of the Histogram.
+   * @param {string} width - Width of the Histogram.
+   * @param {string} paddingLR - The padding on the left and right on the sheet for the histogram
+   */
+  function drawHistogram(height, width, paddingLR) {
+    var indent = (((widthTable + 20) - times.length * width - paddingLR * 2) / (times.length - 1)) + width;
+    var initialX = axisXTable + paddingLR; // px;
     var initialY = axisYTable + heightTable - height - 20;
     var max = getMaxTimes(times);
     var step = height / max; // px;
@@ -34,7 +39,11 @@ window.renderStatistics = function (ctx, names, times) {
       drawColumn(initialX + indent * i, initialY + (height - times[i] * step), width, times[i] * step, names[i], times[i]);
     }
   }
-
+  /**
+   * Get the maximum value from the array.
+   * @param {array} playTimes - Game time of the players.
+   * @return {string} max - Maximum playing time
+   */
   function getMaxTimes(playTimes) {
     var max = 0;
     for (var i = 0; i < playTimes.length; i++) {
@@ -45,7 +54,15 @@ window.renderStatistics = function (ctx, names, times) {
     }
     return max;
   }
-
+  /**
+   * Draws a histogram bar.
+   * @param {number} X - The beginning of the column along the X-axis.
+   * @param {number} Y - The beginning of the column along the Y-axis.
+   * @param {number} width - The width of the column.
+   * @param {number} height - The height of the column.
+   * @param {string} playerName - Player name.
+   * @param {number} playerTime - Playing time.
+   */
   function drawColumn(X, Y, width, height, playerName, playerTime) {
     ctx.fillStyle = getColorColumn(playerName);
     ctx.fillRect(X, Y, width, height);
@@ -54,11 +71,24 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillText(playerName, X + width / 2, Y + height + 20);
     ctx.fillText(Math.floor(playerTime), X + width / 2, Y - 10);
   }
-
+  /**
+   * Gets the color of the column by the name of the player.
+   * @param {string} playerName - The names of the players.
+   * @return {string} playerName - the color of the column
+   */
   function getColorColumn(playerName) {
     return playerName === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255,' + Math.random() + ')';
   }
-
+  /**
+   * Draws the sheet of the histogram.
+   * @param {number} axisX - The beginning of the sheet on the X-axis.
+   * @param {number} axisY - The beginning of the sheet on the Y-axis.
+   * @param {number} width - The width of the sheet.
+   * @param {number} height - The height of the sheet.
+   * @param {number} stepImg - Step pattern border.
+   * @param {string} color1 - The main color of the gradient background sheet.
+   * @param {string} color2 - The additional color of the gradient background sheet.
+   */
   function drawRectangle(axisX, axisY, width, height, stepImg, color1, color2) {
     var tempX = axisX;
     var tempY = axisY;
@@ -90,7 +120,10 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.stroke();
     ctx.fill();
   }
-
+  /**
+   * Places a line through the newline.
+   * @param {array} words - An array of individual lines.
+   */
   function drawText(words) {
     ctx.fillStyle = '#000';
     ctx.font = '16px PT Mono';
