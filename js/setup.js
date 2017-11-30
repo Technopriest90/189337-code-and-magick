a864fa4f8fbd180b2196a829edebbd814a1297a9
Technopriest90 <erokhin.ag@yandex.ru>
2017-11-30 16:54:01 +0300

    Доделал задание, осталось jsdoc

--- js/setup.js
+++ js/setup.js
@@ -7,6 +7,7 @@ var WIZARD_NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var NUMBER_OF_WIZARDS = 4;
 
 wizardsAdd(WIZARD_NAMES, WIZARD_SURNAMES, WIZARD_COAT_COLORS, WIZARD_EYES_COLORS, NUMBER_OF_WIZARDS, wizardTemplate, wizardPlace);
@@ -77,44 +78,109 @@ function wizardsAdd(names, surnames, coatColors, eyesColors, number, template, p
   place.appendChild(temp);
 }
 
-function popupKeypressHandler(element, keypress, method) {
-  element.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === keypress) {
-      method();
-    }
-  });
-}
-
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
-
-function openPopup(window) {
-  return function () {
-    window.classList.remove('hidden');
-  };
-}
-
-function closePopup(window) {
-  return function () {
-    window.classList.add('hidden');
-  };
-}
-
-function openPopupFull(window) {
-  return function (evt) {
-    popupKeypressHandler(document, ESC_KEYCODE, closePopup(window));
-    if (evt.type === 'click') {
-      openPopup(window)();
-    } else if (evt.type === 'focus') {
-      popupKeypressHandler(evt.currentTarget, ENTER_KEYCODE, openPopup(window));
-    }
-  };
-}
-
 var setupOpen = document.querySelector('.setup-open');
+var setupOpenIcon = setupOpen.querySelector('.setup-open-icon');
 var setupClose = document.querySelector('.setup-close');
+var setupUserName = setupWindow.querySelector('.setup-user-name');
+var colorWizardCoat = setupWindow.querySelector('.wizard-coat');
+var colorWizardEyes = setupWindow.querySelector('.wizard-eyes');
+var colorWizardFireball = setupWindow.querySelector('.setup-fireball-wrap');
 
-setupOpen.addEventListener('click', openPopupFull(setupWindow));
-setupOpen.querySelector('.setup-open-icon').addEventListener('focus', openPopupFull(setupWindow));
-setupClose.addEventListener('click', closePopup(setupWindow));
+setupOpen.addEventListener('click', openPopup);
+setupOpenIcon.addEventListener('focus', iconFocusHandler);
+setupClose.addEventListener('click', closePopup);
+setupClose.addEventListener('focus', closeFocusHandler);
+setupUserName.addEventListener('focus', inputFocusHandler);
+setupUserName.addEventListener('blur', inputFocusHandler);
+colorWizardCoat.addEventListener('click', colorClickHandler(colorWizardCoat, WIZARD_COAT_COLORS));
+colorWizardEyes.addEventListener('click', colorClickHandler(colorWizardEyes, WIZARD_EYES_COLORS));
+colorWizardFireball.addEventListener('click', colorFireballClickHandler(colorWizardFireball, WIZARD_FIREBALL_COLORS));
+/**
+ * The event handler on closing pop-up with the Esc button.
+ * @param {object} evt - event
+ */
+function popupKeydownEscHandler(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+}
+/**
+ * The event handler on opening pop-up with the Enter button.
+ * @param {object} evt - event
+ */
+function popupKeydownEnterHandler(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+}
+/**
+ * The event handler on closing pop-up with the Enter button.
+ * @param {object} evt - event
+ */
+function popupKeydownEnterCloseHandler(evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+}
+/**
+ * Event handler for opening a popup.
+ */
+function openPopup() {
+  setupWindow.classList.remove('hidden');
+  document.addEventListener('keydown', popupKeydownEscHandler);
+}
+/**
+ * The event handler adding an event handler on the event object. This event handler is required for open a pop-up by pressing enter.
+ * @param {object} evt - event
+ */
+function iconFocusHandler(evt) {
+  evt.currentTarget.addEventListener('keydown', popupKeydownEnterHandler);
+}
+/**
+ * The event handler adding an event handler on the event object. This event handler is required for close a pop-up by pressing enter.
+ * @param {object} evt - event
+ */
+function closeFocusHandler(evt) {
+  evt.currentTarget.addEventListener('keydown', popupKeydownEnterCloseHandler);
+}
+/**
+ * The event handler adds or removes an event handler on the document depending on the type of event.
+ * @param {object} evt - event
+ */
+function inputFocusHandler(evt) {
+  if (evt.type === 'focus') {
+    document.removeEventListener('keydown', popupKeydownEscHandler);
+  } else if (evt.type === 'blur') {
+    document.addEventListener('keydown', popupKeydownEscHandler);
+  }
+}
+/**
+ *Event handler for closing a popup.
+ */
+function closePopup() {
+  setupWindow.classList.add('hidden');
+}
+/**
+ * Event handler for closing a popup.
+ * param {object} item -
+ * param {array} colors -
+ */
+function colorClickHandler(item, colors) {
+  return function () {
+    item.style.fill = getRandomElement(colors);
+  };
+}
+/**
+ * Event handler for closing a popup.
+ * param {object} item -
+ * param {array} colors -
+ */
+function colorFireballClickHandler(item, colors) {
+  return function () {
+    item.style.backgroundColor = getRandomElement(colors);
+  };
+}
+
 
